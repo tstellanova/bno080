2a52c604c278a87835ebef5c78cb842d96dd4b87
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-02-20 13:07:15 -0800

    Restructuring tests

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,23 +1,11 @@
 pub mod i2c;
 pub mod spi;
 
+#[cfg(test)]
+pub mod mock_i2c_port;
+
 use core::ops::{Shl};
 
-// #[derive(Debug)]
-// pub enum CommBusError {
-//     /// I2C bus error
-//     // I2c(E),
-//     // Spi(E),
-//
-//     /// Invalid sensor product ID was read
-//     InvalidChipId(u8),
-//
-//     /// Unsupported sensor firmware version
-//     InvalidFWVersion(u8),
-//
-//     /// Not enough data available to fulfill the read
-//     NoDataAvailable(u8),
-// }
 
 
 /// A method of communicating with the sensor
@@ -39,6 +27,9 @@ pub trait SensorInterface {
 pub use self::i2c::I2cInterface;
 pub use self::spi::SpiInterface;
 
+#[cfg(test)]
+pub use self::mock_i2c_port::FakeI2cPort;
+
 
 pub(crate) const PACKET_HEADER_LENGTH: usize = 4;
 
@@ -49,15 +40,15 @@ struct SensorCommon {
 
 impl SensorCommon {
     fn parse_packet_header(packet: &[u8]) -> usize {
-        const CONTINUATION_FLAG_CLEAR: u16 = !(0x80);
+        const CONTINUATION_FLAG_MASK: u16 = 0x80;
+        const CONTINUATION_FLAG_CLEAR: u16 = !(CONTINUATION_FLAG_MASK);
         if packet.len() < PACKET_HEADER_LENGTH {
             return 0;
         }
         //Bits 14:0 are used to indicate the total number of bytes in the body plus header
         //maximum packet length is ... 32767?
-        let raw_pack_len: u16 =
-            (packet[0] as u16) + ((packet[1] as u16)
-                & CONTINUATION_FLAG_CLEAR).shl(8);
+        let raw_pack_len: u16 = (packet[0] as u16)
+            + ((packet[1] as u16)  & CONTINUATION_FLAG_CLEAR).shl(8);
         let packet_len: usize = raw_pack_len as usize;
 
         //let is_continuation:bool = (packet[1] & 0x80) != 0;
@@ -65,5 +56,68 @@ impl SensorCommon {
         //let seq_num =  packet[3];
 
         packet_len
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::interface::SensorCommon;
+    use super::*;
+    use core::ops::{Shr};
+
+    #[test]
+    fn test_parse_packet_header() {
+        let short_packet: [u8; 2] = [13, 15];
+        let size = SensorCommon::parse_packet_header(&short_packet);
+        assert_eq!(0, size, "truncated packet header should have length zero");
+
+        let long_packet_len: usize = 1024;
+        let mut raw_packet: [u8; PACKET_HEADER_LENGTH] = [
+            (long_packet_len & 0xFF) as u8,
+            long_packet_len.shr(8) as u8,
+            0,
+            0
+        ];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, long_packet_len, "verify > 255 packet length");
+
+        //now set the continuation flag
+        raw_packet[1] = 0x80 | raw_packet[1];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, long_packet_len, "verify continuation packet");
+
+        let short_packet_len: usize = 36;
+        raw_packet = [
+            (short_packet_len & 0xFF) as u8,
+            short_packet_len.shr(8) as u8,
+            0,
+            0
+        ];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, short_packet_len, "verify short packet");
+
+        raw_packet[1] = 0x80 | raw_packet[1];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, short_packet_len, "verify short packet continuation");
+
+        // first (uncontinued) packet
+        raw_packet = [
+            20 as u8,
+            1 as u8,
+            0,
+            0
+        ];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, 276, "verify > 255 packet length");
+
+        //from actual received packet
+        raw_packet = [
+            19 as u8,
+            129 as u8,
+            0,
+            1
+        ];
+        let size = SensorCommon::parse_packet_header(&raw_packet);
+        assert_eq!(size, 275, "verify > 255 packet length");
     }
 }
