90da35e9c3f8359567940f759032b51901e94c46
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-04-17 12:42:35 -0700

    Combine cfg flags

--- src/macros.rs
+++ src/macros.rs
@@ -2,15 +2,13 @@
 
 
 #[macro_export]
-#[cfg(debug_assertions)]
-#[cfg(test)]
+#[cfg(all(debug_assertions, test))]
 macro_rules! debug_println {
     ($s:expr, $($tt:tt)*) => ({ })
 }
 
 #[macro_export]
-#[cfg(debug_assertions)]
-#[cfg(not(test))]
+#[cfg(all(debug_assertions, not(test)))]
 macro_rules! debug_println {
     ($($arg:tt)*) => ({
         use cortex_m_semihosting::hprintln;
